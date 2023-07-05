1592b360b4975abfc8e9c4dd708900bacaa03474
算神 <lishewen@msn.com>
2023-07-05 15:01:37 +0800

    update to .net 7

--- SignalRSimpleSample/Startup.cs
+++ SignalRSimpleSample/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using SignalRSimpleSample.Hubs;
@@ -26,22 +27,23 @@ namespace SignalRSimpleSample
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSignalR();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             app.UseWebSockets();
-            app.UseSignalR(routes =>
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
             {
-                routes.MapHub<TestMessagesHub>("/testmessages");
+                endpoints.MapHub<TestMessagesHub>("/testmessages");
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
-            app.UseMvc();
         }
     }
 }
